35c08eb098c2733ae87e5b79eebb344fc73f2042
Nurdina <43734373+nurdinaDn@users.noreply.github.com>
2018-10-09 23:20:11 +0800

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,20 @@
+
+
+public class Node {
+	String label;
+	int value;
+	
+	void getLabel() {
+		System.out.println(label);
+	}
+	void setLabel() {
+		System.out.println(label);
+	}
+	void getValue() {
+		System.out.println(value);
+	}
+	void setValue() {
+		System.out.println(value);
+	}
+
+}
