11f69ca06fa5e9d142807c5289befba75bfa4c9f
Nurdina <43734373+nurdinaDn@users.noreply.github.com>
2018-10-09 23:17:47 +0800

    Create Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,53 @@
+public class Mahasiswa {
+	 String nama;
+	 String nim;
+	 String alamat;
+	 char golonganDarah;
+	 boolean status;// True = aktif false = tidak aktif
+	 float tinggiBadan;
+	 float beratBadan;
+	 
+	void getNama() {
+		System.out.println(nama);
+	}
+	void setNama() {
+		System.out.println(nama);
+	}
+	void getNim() {
+		System.out.println(nim);
+	}
+	void setNim() {
+		System.out.println(nim);
+	}
+	void getAlamat() {
+		System.out.println(alamat);
+	}
+	void setAlamat() {
+		System.out.println(alamat);
+	}
+	void getGolonganDarah() {
+		System.out.println(golonganDarah);
+	}
+	void setGolonganDarah() {
+		System.out.println(golonganDarah);
+	}
+	void getStatus() {
+		System.out.println(status);
+	}
+	void setStatus() {
+		System.out.println(status);
+	}
+	void getTinggiBadan() {
+		System.out.println(tinggiBadan);
+	}
+	void setTinggiBadan() {
+		System.out.println(tinggiBadan);
+	}
+	void getBeratBadan() {
+		System.out.println(beratBadan);
+	}
+	void setBeratBadan() {
+		System.out.println(beratBadan);
+	}
+
+}
