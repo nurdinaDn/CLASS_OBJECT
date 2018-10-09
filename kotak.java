b2e3ba2a23eba7b926abcd4ed0b19b0d55525a75
Nurdina <43734373+nurdinaDn@users.noreply.github.com>
2018-10-09 23:16:15 +0800

    Create kotak.java

---
+++ kotak.java
@@ -0,0 +1,27 @@
+public class kotak {
+private double panjang, lebar, luas, keliling;
+	void getPanjang() {
+		System.out.println(panjang);
+	}
+	void getLebar() {
+		System.out.println(lebar);
+	}
+	void getLuas() {
+		System.out.println(panjang * lebar);
+	}
+	void getKeliling() {
+		System.out.println(2 * (panjang + lebar));
+	}
+	void setPanjang() {
+		System.out.println(panjang);
+	}
+	void setLebar() {
+		System.out.println(lebar);
+	}
+	void setLuas() {
+		System.out.println(panjang * lebar);
+	}
+	void setKeliling() {
+		System.out.println(2 * (panjang + lebar));
+	}
+}
